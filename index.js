45351c01d743b1d0d91ecb79532d4d1bf603e139
King Elisha <ek.chibueze@gmail.com>
2020-01-21 08:55:18 +0100

    fill method

--- index.js
+++ index.js
@@ -3,9 +3,27 @@ class Matrix {
   constructor(rows = 0, cols = 0) {
     this.rows = rows;
     this.cols = cols;
+
+    /** 
+     * @type {number[][]} entries 
+     */
+    this.entries = new Array(rows);
+    this.entries.fill(new Array(cols));
+  }
+
+  /**
+   * Fill matrix with a particular number
+   * @param {number} num
+   */
+  fill(num) {
+    this.entries.forEach((col) => col = col.fill(num));
+
+    return this;
   }
 
   toString() {
-    return `Matrix(rows: ${this.rows}, cols: ${this.cols})`;
+    return `Matrix(rows: ${this.rows}, cols: ${this.cols}, entries: ${this.entries})`;
   }
 }
+
+console.log(new Matrix(4, 20).fill(2));
