b7b6fa5511995c87714ca775cf3c906206eb943d
King Elisha <ek.chibueze@gmail.com>
2020-02-28 12:43:07 +0100

    concurrency

---
+++ utils/concurrency.js
@@ -0,0 +1,28 @@
+class ConcurrentTaskQueue {
+  constructor(taskPromisesFunc = [], batchSize = 1) {
+    this.batchSize =
+      batchSize > taskPromisesFunc.length ? taskPromisesFunc.length : batchSize;
+    this.todoTasks = taskPromisesFunc;
+    this.resolvedValues = [];
+  }
+
+  run(resolve, reject) {
+    if (this.todoTasks.length > 0) {
+      const taskPromises = this.todoTasks.splice(0, this.batchSize);
+      Promise.all(taskPromises.map(p => p()))
+        .then(resolvedValues => {
+          this.resolvedValues = [...this.resolvedValues, ...resolvedValues];
+          this.run(resolve, reject);
+        })
+        .catch(err => reject(err));
+    } else {
+      resolve(this.resolvedValues);
+    }
+  }
+
+  runTasks() {
+    return new Promise((resolve, reject) => {
+      this.run(resolve, reject);
+    });
+  }
+}
